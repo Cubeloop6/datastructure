d0e329d07958a4c12c4a118e0f20174c9fdfb78d
Cubeloop6 <rockiseverything12345@gmail.com>
2020-11-25 11:28:16 -0800

    commit

---
+++ data structure/src/binaryTree/StacksRFun.java
@@ -0,0 +1,19 @@
+package binaryTree;
+
+
+
+public class StacksRFun {
+public static void main(String[] args) {
+		
+		Stack nums = new Stack();
+		nums.push(15);
+		nums.push(8);
+		nums.push(10);
+		
+		System.out.println(nums.pop());
+		
+		nums.show();
+		
+		
+	}
+}
